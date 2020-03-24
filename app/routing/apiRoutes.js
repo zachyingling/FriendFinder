96fe727cf8579934b7d9d5ed474e1eb5ddc73b2f
zachary <zpyingling9559@gmail.com>
2020-03-24 16:01:47 -0400

    being able to find the index of the best matched friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,12 +7,13 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", (req, res) => {
-    let friendsScores = [];
     let userScores = [];
     let totalDifference = [];
 
+    // Puts user inputs into an array
     userScores = Object.values(req.body);
 
+    // Put total difference of all friends into an array called totalDifference
     for (const element of friends.friends) {
       let tempDifference = 0;
       for (let i = 0; i < element.scores.length; i++) {
@@ -21,6 +22,17 @@ module.exports = function(app) {
         );
       }
       totalDifference.push(tempDifference);
+    }
+
+    // Finds the lowest number
+    let lowestNumber = Math.min.apply(Math, totalDifference);
+    let indexFlag = -1;
+
+    for (let i = 0; i < totalDifference.length; i++) {
+      if (lowestNumber === totalDifference[i]) {
+        indexFlag = i;
+        break;
+      }
     }
 
     res.json(true);
