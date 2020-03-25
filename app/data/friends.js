619e7a33e07e176542c5cfdc82ea9c00db131f21
zachary <zpyingling9559@gmail.com>
2020-03-24 21:31:39 -0400

    fixing up the modal and the friends images and scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,43 +3,50 @@ module.exports = {
     {
       name: "Ahmed",
       photo:
-        "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "https://fashionweekdaily.com/wp-content/uploads/2019/09/DanWakeford_HTH-51.jpg",
       scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
       name: "Jacob Deming",
       photo:
-        "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
       scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
     },
     {
       name: "Jeremiah Scanlon",
-      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-      scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
+      photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdNNo5a11ntpKojLVaTxN0uQtTWFuWR-Hizf55ZwRJJRNQwmNz",
+      scores: ["2", "3", "2", "2", "4", "5", "3", "2", "1", "3"]
     },
     {
       name: "Louis T. Delia",
       photo:
-        "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-      scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
+        "https://static.boredpanda.com/blog/wp-content/uploads/2018/12/ai-image-generation-fake-faces-people-nvidia-5c18b20b472c2__700.jpg",
+      scores: ["3", "3", "4", "5", "3", "1", "3", "2", "2", "3"]
     },
     {
       name: "Lou Ritter",
       photo:
-        "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-      scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
+        "https://nerdist.com/wp-content/uploads/2017/10/Nvidia-AI-Celebrity-Face-Body-Image-10302017.jpg",
+      scores: ["4", "3", "4", "2", "5", "2", "5", "3", "1", "4"]
     },
     {
       name: "Jordan Biason",
       photo:
-        "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "https://static.boredpanda.com/blog/wp-content/uploads/2018/12/ai-image-generation-fake-faces-people-nvidia-5c18acb9cd17f__700.jpg",
       scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
     },
     {
-      name: "Branden Patten (https://github.com/pattenbranden)",
+      name: "Branden Patten",
       photo:
-        "https://avatars0.githubusercontent.com/u/57732191?s=460&u=a76cde823ee154753799eed9507b163f75cc8975&v=4",
+        "https://cdn.i.haymarketmedia.asia/?n=asian-investor%2Fcontent%2FMichael+Han+-+Brightsphere+-+crop+landscape.jpg&h=570&w=855&q=100&v=20190520&c=1",
       scores: ["5", "3", "4", "1", "4", "2", "4", "1", "2", "1"]
+    },
+    {
+      name: "Josh Gallagher",
+      photo:
+        "https://images-na.ssl-images-amazon.com/images/I/718N-NSEJpL._AC_SL1500_.jpg",
+      scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
     }
   ]
 };
